19c160865c1a760542ba463e628f32e68bd93493
Nadiia <shulltea113@gmail.com>
2024-03-23 11:39:17 +0100

    style footer

--- homeworks_128/term_project/src/components/footer/Footer.jsx
+++ homeworks_128/term_project/src/components/footer/Footer.jsx
@@ -5,21 +5,21 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer>
-      <div className="footer-container">
-        <div className="logo-social-info">
+      <div className="footer--container">
+        <div className="footer--logo-social-info">
           <Logo />
-          <p>
+          <p className="footer--logo-desc">
             It is a long established fact that a reader will be distracted
             lookings.
           </p>
-          <div className="social-media">
+          <div className="footer--social-media">
             <img src="src/assets/images/icons/facebook.svg" alt="facebook" />
             <img src="src/assets/images/icons/twitter.svg" alt="twitter" />
             <img src="src/assets/images/icons/linkedin.svg" alt="linkedin" />
             <img src="src/assets/images/icons/instagram.svg" alt="instagram" />
           </div>
         </div>
-        <div className="footer-nav-container">
+        <div className="footer--nav-container">
           <h4>Pages</h4>
           <nav>
             <ul>
@@ -31,7 +31,7 @@ const Footer = () => {
             </ul>
           </nav>
         </div>
-        <div className="footer-nav-container">
+        <div className="footer--nav-container">
           <h4>Services</h4>
           <nav>
             <ul>
@@ -43,14 +43,14 @@ const Footer = () => {
             </ul>
           </nav>
         </div>
-        <div className="footer-nav-container">
+        <div className="footer--nav-container">
           <h4>Contact</h4>
           <p>55 East Birchwood Ave. Brooklyn, New York 11201 </p>
           <p>contact@selhono.com</p>
           <p>(123) 456 - 7890</p>
         </div>
       </div>
-      <p className="copy">Copyright © SELHONO</p>
+      <p className="footer--copy">Copyright © SELHONO</p>
     </footer>
   );
 };
